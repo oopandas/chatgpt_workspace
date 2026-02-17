60194c373da8a4f050af28e83ed7bc855d15695a
oopandas <oopandas@users.noreply.github.com>
2026-02-17 10:56:31 +0900

    Serialize ChatGPT data to JSON and send it to the API via POST

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -13,3 +13,11 @@ setTimeout(() => {
         console.log("要素が見つかりません");
     }
 }, 3000);
+
+fetch("http://127.0.0.1:8000", {
+    method: "POST",//django側の上のurls.pyのリンクにPOSTリクエストを送る
+    headers: {
+        "Content-Type": "aplication/json"//JSON形式という説明
+    },
+    body: JSON.stringify()//文字列に変換
+})
