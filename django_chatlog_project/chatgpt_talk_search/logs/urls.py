2e242a53645a142753b878055a0e1f5339563099
oopandas <oopandas@users.noreply.github.com>
2026-04-26 11:19:26 +0900

    Remove unused URL patterns after removing CRUD views

--- django_chatlog_project/chatgpt_talk_search/logs/urls.py
+++ django_chatlog_project/chatgpt_talk_search/logs/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from . import views
-from logs.views import LogsCreate
 
 app_name = "logs"
 
 urlpatterns = [
-    path("", views.logs_view, name="logs"),
-    path("list/", views.logs_list, name="logs_list"),
-    path("detail/<int:pk>/", views.logs_detail, name="logs_detail"),
-    path("create/", LogsCreate.as_view(), name="create"),
     path("upload_zip/", views.upload_zip, name="upload_zip"),
 ]
