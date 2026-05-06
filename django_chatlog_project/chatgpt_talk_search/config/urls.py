8eda776e09d82d7cc6dd60f800fb3c904549b7cb
oopandas <oopandas@users.noreply.github.com>
2026-05-06 14:00:19 +0900

    Add redirect from root URL to log upload page

--- django_chatlog_project/chatgpt_talk_search/config/urls.py
+++ django_chatlog_project/chatgpt_talk_search/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    # ルートURLにアクセスしたときにログアップロードページにリダイレクト
+    path("", lambda request: redirect("logs:upload_zip")),  
     path("admin/", admin.site.urls),
     path("logs/", include("logs.urls")),
 ]
